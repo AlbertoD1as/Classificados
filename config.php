624f77e846e65d2e1443b4ba0ed8cdac67d0027f
Alberto Santos Dias <albertosantosdias@hotmail.com>
2021-11-23 23:20:11 -0300

    Tela de cadastrar

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php 
+session_start();
+
+try {
+    $pdo = new PDO("mysql:dbname=classificados;host=localhost", "root", "");
+} catch(PDOException $e) {
+    echo "FALHOU: ".$e->getMessage();
+    exit;
+}
+
+
+?>
--- pages/header.php
+++ pages/header.php
@@ -1,3 +1,4 @@
+<?php require 'config.php'; ?>
 <html>
     <head> 
        
@@ -17,8 +18,13 @@
                 <a href="./" class="navbar-brand">Classificados</a>
             </div>  
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="">Cadastre-se</a></li>
-                <li><a href="">Login</a></li>
+                <?php if(isset($_SESSION['cLogin']) && !empty($_SESSION['cLogin'])): ?>
+                <li><a href="meus-anuncios.php">Meus Anúncios</a></li>
+                <li><a href="sair.php">Sair</a></li>
+                <?php else: ?>
+                    <li><a href="cadastre-se.php">Cadastre-se</a>
+                    <li><a href="login.php">Login</a></li>
+                    <?php endif; ?>
             </ul> 
         </div>
     </nav>
