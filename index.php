40104446605f9ac6365f42e1bf3b357a71ba64b4
Alberto Santos Dias <albertosantosdias@hotmail.com>
2021-12-01 23:18:27 -0300

    include/require

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-<?php require 'pages/header.php'; ?>
+ <?php include 'pages/header.php'; ?>
 
         <div class="container-fluid">
             <div class="jumbotron">
                 <h2>Nós temos hoje 999 anúncios.</h2>
                 <p>E mais de 999 usuários cadastrados.</p>
             </div>
-
+            <div class="container">   
             <div class="row">
                 <div class="col-sm-3">
                     <h4>Pesquisa Avançada</h4>
